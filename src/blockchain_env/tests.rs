ee8d7cf8b5b2f3af9f0042fc68486ece363ed037
aaryan-gulia <aaryan.gulia@gmail.com>
2024-04-30 23:18:57 +0300

    transaction enum 1st test

--- src/blockchain_env/tests.rs
+++ src/blockchain_env/tests.rs
@@ -1,1 +1,25 @@
-
+use crate::blockchain_env::transaction::Transaction;
+#[test]
+fn new_transaction() {
+    let t = Transaction::Normal {
+        id: 1,
+        gas_amount: 1.0,
+        max_mev_amount: 1.0,
+    };
+    assert!(match t {
+        Transaction::Normal {
+            id,
+            gas_amount,
+            max_mev_amount,
+        } => {
+            false
+        }
+        Transaction::Attack {
+            id,
+            gas_amount,
+            max_mev_amount,
+        } => {
+            false
+        }
+    });
+}
