f180992c3aa57598e4b8001aca709bf4d812c9aa
aaryan-gulia <aaryan.gulia@gmail.com>
2024-05-03 13:03:24 +0300

    transaction enum -> transaction struct

--- src/blockchain_env/transaction.rs
+++ src/blockchain_env/transaction.rs
@@ -1,12 +1,11 @@
-pub enum Transaction {
-    Normal {
-        id: u32,
-        gas_amount: f64,
-        max_mev_amount: f64,
-    },
-    Attack {
-        id: u32,
-        gas_amount: f64,
-        max_mev_amount: f64,
-    },
+pub struct Transaction {
+    id: u32,
+    pub gas_amount: f64,
+    pub max_mev_amount: f64,
+    pub transaction_type: TransactionType,
+}
+
+pub enum TransactionType {
+    Normal,
+    Attack,
 }
