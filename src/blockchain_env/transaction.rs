b3bcdd51342517d869d69433eb010a4d1383c3fd
aaryan-gulia <aaryan.gulia@gmail.com>
2024-05-03 14:31:35 +0300

    transaction builder pattern with new fn

--- src/blockchain_env/transaction.rs
+++ src/blockchain_env/transaction.rs
@@ -9,3 +9,21 @@ pub enum TransactionType {
     Normal,
     Attack,
 }
+
+pub struct TransactionBuilder {
+    id: Option<u32>,
+    gas_amount: Option<f64>,
+    max_mev_amount: Option<f64>,
+    transaction_type: Option<TransactionType>,
+}
+
+impl TransactionBuilder {
+    pub fn new() -> Self {
+        TransactionBuilder {
+            id: None,
+            gas_amount: None,
+            max_mev_amount: None,
+            transaction_type: None,
+        }
+    }
+}
