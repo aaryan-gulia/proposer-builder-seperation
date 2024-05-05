74b47226e45321f1e4be1aa392c8676391e889e0
aaryan-gulia <aaryan.gulia@gmail.com>
2024-05-06 01:00:06 +0300

    builder mempool rng

--- src/entities/builder.rs
+++ src/entities/builder.rs
@@ -1,5 +1,7 @@
 use crate::blockchain_env::transaction;
 use crate::entities::traits;
+use rand::distributions::{Distribution, Uniform};
+use rand::Rng;
 
 pub struct Builder {
     pub builder_id: u32,
@@ -16,8 +18,12 @@ impl Builder {
         }
     }
     pub fn collect_transaction(&mut self, transaction_vec: &Vec<transaction::Transaction>) {
+        let mut rng = rand::thread_rng();
+        let dist = Uniform::from(0.0..100.0);
         for t in transaction_vec {
-            self.mempool.push(*t);
+            if self.characteristic * 100.0 > dist.sample(&mut rng) {
+                self.mempool.push(*t);
+            }
         }
     }
 }
