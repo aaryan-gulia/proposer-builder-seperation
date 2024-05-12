dcd439b2e0c0c87cd840723b18352b007a452982
aaryan-gulia <aaryan.gulia@gmail.com>
2024-05-12 17:14:11 +0300

    build_block moved to NormalBuilder impl

--- src/entities/builder.rs
+++ src/entities/builder.rs
@@ -1,29 +1,37 @@
 use crate::blockchain_env::block;
 use crate::blockchain_env::transaction;
-use crate::entities::traits;
+use crate::entities::{proposer, traits};
 use rand::distributions::{Distribution, Uniform};
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use std::collections::HashSet;
 
-struct NormalBuilder;
-struct MevBuilder;
 struct NormalBuilderProposer;
 struct MevBuilderPorposer;
 
-impl traits::Proposer for NormalBuilderProposer {}
-impl traits::Proposer for MevBuilderPorposer {}
+pub enum BuilderType {
+    NormalBuilder(NormalBuilder),
+    MevBuilder(MevBuilder),
+}
 
+struct NormalBuilder {
+    pub builder: Builder,
+    pub proposer: Option<proposer::Proposer>,
+}
+struct MevBuilder {
+    pub builder: Builder,
+    pub proposer: Option<proposer::Proposer>,
+}
 pub struct Builder {
-    pub builder_id: u32,
+    pub id: u32,
     pub characteristic: f64,
     pub mempool: HashSet<transaction::Transaction>,
 }
 
 impl Builder {
-    pub fn new(builder_id: u32, characteristic: f64) -> Self {
+    pub fn new(id: u32, characteristic: f64) -> Self {
         Builder {
-            builder_id,
+            id,
             characteristic,
             mempool: vec![].into_iter().collect(),
         }
@@ -40,55 +48,6 @@ impl Builder {
 
     pub fn clean_mempools(&mut self, remove_transactions: &HashSet<transaction::Transaction>) {
         self.mempool.retain(|t| !remove_transactions.contains(t));
-    }
-    pub fn build_block(
-        &self,
-        mut block_size: u32,
-        blockchain: &Vec<block::Block>,
-        random_numbers: &Vec<f64>,
-    ) -> block::Block {
-        let mut gas_vec: Vec<transaction::Transaction> = vec![];
-        gas_vec.reserve(self.mempool.len());
-        for t in self.mempool.iter() {
-            gas_vec.push(*t);
-        }
-        gas_vec.sort_unstable_by(transaction::Transaction::compare_transaction_by_gas);
-        if block_size > gas_vec.len() as u32 {
-            block_size = gas_vec.len() as u32;
-        }
-        let mev_captured = 0;
-        let mut gas_captured = 0;
-        let mut transactions_in_block: HashSet<transaction::Transaction> =
-            vec![].into_iter().collect();
-        for i in 0..std::cmp::min(block_size as usize, gas_vec.len()) {
-            gas_captured += gas_vec[i as usize].gas_amount;
-            transactions_in_block.insert(gas_vec[i as usize].clone());
-        }
-        let mut bid: f64 = 0.0;
-        if blockchain.len() > 10 {
-            bid = Builder::calculate_bid(
-                gas_captured + mev_captured,
-                &blockchain[blockchain.len() - 10..],
-                10,
-                random_numbers,
-            );
-        } else if blockchain.len() == 0 {
-            bid = ((gas_captured + mev_captured) / 2) as f64
-        } else {
-            bid = Builder::calculate_bid(
-                gas_captured + mev_captured,
-                &blockchain[..],
-                blockchain.len() as u32,
-                random_numbers,
-            )
-        }
-        block::Block::new(
-            self.builder_id,
-            gas_captured as f64,
-            mev_captured as f64,
-            bid,
-            transactions_in_block,
-        )
     }
     pub fn calculate_bid(
         block_value: i64,
@@ -124,5 +83,57 @@ impl Builder {
             }
         }
         optimal_bid as f64
+    }
+}
+
+impl NormalBuilder {
+    pub fn build_block(
+        &self,
+        mut block_size: u32,
+        blockchain: &Vec<block::Block>,
+        random_numbers: &Vec<f64>,
+    ) -> block::Block {
+        let mut gas_vec: Vec<transaction::Transaction> = vec![];
+        gas_vec.reserve(self.builder.mempool.len());
+        for t in self.builder.mempool.iter() {
+            gas_vec.push(*t);
+        }
+        gas_vec.sort_unstable_by(transaction::Transaction::compare_transaction_by_gas);
+        if block_size > gas_vec.len() as u32 {
+            block_size = gas_vec.len() as u32;
+        }
+        let mev_captured = 0;
+        let mut gas_captured = 0;
+        let mut transactions_in_block: HashSet<transaction::Transaction> =
+            vec![].into_iter().collect();
+        for i in 0..std::cmp::min(block_size as usize, gas_vec.len()) {
+            gas_captured += gas_vec[i as usize].gas_amount;
+            transactions_in_block.insert(gas_vec[i as usize].clone());
+        }
+        let mut bid: f64 = 0.0;
+        if blockchain.len() > 10 {
+            bid = Builder::calculate_bid(
+                gas_captured + mev_captured,
+                &blockchain[blockchain.len() - 10..],
+                10,
+                random_numbers,
+            );
+        } else if blockchain.len() == 0 {
+            bid = ((gas_captured + mev_captured) / 2) as f64
+        } else {
+            bid = Builder::calculate_bid(
+                gas_captured + mev_captured,
+                &blockchain[..],
+                blockchain.len() as u32,
+                random_numbers,
+            )
+        }
+        block::Block::new(
+            self.builder.id,
+            gas_captured as f64,
+            mev_captured as f64,
+            bid,
+            transactions_in_block,
+        )
     }
 }
