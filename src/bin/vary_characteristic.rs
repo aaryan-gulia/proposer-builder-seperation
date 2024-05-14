10db5dbbcad52ea6b69c37a93f92f8f5e0cd982f
aaryan-gulia <aaryan.gulia@gmail.com>
2024-05-14 12:39:54 +0300

    pos simulation ready

--- src/bin/vary_characteristic.rs
+++ src/bin/vary_characteristic.rs
@@ -10,7 +10,7 @@ use std::collections::HashSet;
 use std::thread::available_parallelism;
 
 fn main() {
-    vary_characteristic();
+    vary_pos_characteristic();
 }
 
 fn vary_characteristic() {
@@ -40,11 +40,48 @@ fn vary_characteristic() {
                 &rand_num_vec,
             );
             let file_name = format!(
-                "../data/vary_mev_and_characteristic/mev_builders={}characteristic={}.csv",
+                "../data/pos_vary_mev_and_characteristic/mev_builders={}characteristic={}.csv",
                 mev_builders, characteristic
             );
 
             save_continuous_simulation_to_csv(&blockchain, &file_name)
+                .expect("save_blockchain_to_csv() failing from simple_pbs() test");
+
+            pb.inc(1);
+        }
+    });
+    pb.finish();
+}
+
+fn vary_pos_characteristic() {
+    let NUM_BUILDERS: u32 = 50;
+    let MEV_BUILDERS: u32 = 50;
+    let NUM_TRANSACTIONS = 1000;
+    let NUM_BLOCKS = 5000;
+
+    let rand_num_vec: Vec<f64> = get_random_numbers::<f64>(100000000, 0.0, 1.0);
+    let pb = ProgressBar::new((MEV_BUILDERS * 10) as u64);
+    let num_builders_vec: Vec<u32> = (1..=MEV_BUILDERS).collect(); // Parallelize the outer loop iterating over num_builders
+    num_builders_vec.into_par_iter().for_each(|mev_builders| {
+        for characteristic in 1..=5 {
+            let characteristic: f64 = characteristic as f64 / 5.0;
+            let mut builder_vec = initiate_builders(NUM_BUILDERS - mev_builders, characteristic);
+            builder_vec.append(&mut initiate_mev_builder(mev_builders, characteristic));
+
+            let transaction_set: HashSet<transaction::Transaction> =
+                initiate_transactions_default(NUM_TRANSACTIONS);
+
+            let blockchain = execute_pos_simulation(
+                NUM_BLOCKS,
+                &mut builder_vec,
+                transaction_set.clone(), // Clone required for parallel execution
+            );
+            let file_name = format!(
+                "../data/pos_vary_mev_and_characteristic/mev_builders={}characteristic={}.csv",
+                mev_builders, characteristic
+            );
+
+            save_pos_to_csv(&blockchain, &file_name)
                 .expect("save_blockchain_to_csv() failing from simple_pbs() test");
 
             pb.inc(1);
--- src/simulation.rs
+++ src/simulation.rs
@@ -6,6 +6,45 @@ use rand::thread_rng;
 use rand_distr::{Distribution, Normal, NormalError, Uniform};
 use std::collections::HashSet;
 
+pub fn execute_pos_simulation(
+    num_blocks: u32,
+    builder_vec: &mut Vec<builder::BuilderType>,
+    mut transaction_set: HashSet<transaction::Transaction>,
+) -> Vec<block::Block> {
+    let mut blockchain: Vec<block::Block> = vec![];
+    let num_transactions = transaction_set.len() as u32;
+    let mut rng = thread_rng;
+    let uniform = Uniform::new(0, builder_vec.len() as u32);
+
+    for block_index in 1..=num_blocks {
+        for b in builder_vec.iter_mut() {
+            match b {
+                builder::BuilderType::NormalBuilder(b) => {
+                    b.builder.collect_transaction(&transaction_set)
+                }
+                builder::BuilderType::MevBuilder(b) => {
+                    b.builder.collect_transaction(&transaction_set)
+                }
+            }
+        }
+
+        let block_builder = uniform.sample(&mut rng());
+        let b = &builder_vec[block_builder as usize];
+        let mut curr_block = match b {
+            builder::BuilderType::NormalBuilder(b) => b.build_block(100),
+            builder::BuilderType::MevBuilder(b) => b.build_block(100),
+        };
+
+        transaction::Transaction::clean_transaction_set(
+            &mut transaction_set,
+            &curr_block.transactions,
+        );
+        maintain::refill_transactions_default(num_transactions, &mut transaction_set);
+        blockchain.push(curr_block);
+    }
+    blockchain
+}
+
 pub fn execute_simulation(
     num_blocks: u32,
     builder_vec: &mut Vec<builder::BuilderType>,
