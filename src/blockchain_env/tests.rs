539528c5b57ba2316a789058c701b0746b04ef7e
aaryan-gulia <aaryan.gulia@gmail.com>
2024-05-22 19:29:15 +0300

    removed old test for block

--- src/blockchain_env/tests.rs
+++ src/blockchain_env/tests.rs
@@ -22,14 +22,14 @@ fn new_correct_transaction() {
     assert_eq!(t.get_transaction_id().unwrap(), 1);
 }
 
-#[test]
-fn new_block() {
-    let mut b1 = block::Block::new(1, 1.0, 1.0, 1.0, vec![].into_iter().collect());
-    b1.add_to_chain(3);
-    assert_eq!(b1.builder_id, 1);
-    assert_eq!(b1.get_block_index().unwrap(), 1);
-    let mut b2 = block::Block::new(1, 1.0, 1.0, 1.0, vec![].into_iter().collect());
-    assert_eq!(b2.get_block_index(), None);
-    b2.add_to_chain(3);
-    assert_eq!(b2.get_block_index().unwrap(), 2);
-}
+// #[test]
+// fn new_block() {
+//     let mut b1 = block::Block::new(1, 1.0, 1.0, 1.0, vec![].into_iter().collect());
+//     b1.add_to_chain(3);
+//     assert_eq!(b1.builder_id, 1);
+//     assert_eq!(b1.get_block_index().unwrap(), 1);
+//     let mut b2 = block::Block::new(1, 1.0, 1.0, 1.0, vec![].into_iter().collect());
+//     assert_eq!(b2.get_block_index(), None);
+//     b2.add_to_chain(3);
+//     assert_eq!(b2.get_block_index().unwrap(), 2);
+// }
