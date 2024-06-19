90f3429e53b4800d4e057416604eb2445eeff684
aaryan-gulia <aaryan.gulia@gmail.com>
2024-06-19 16:22:41 +0300

    getting stuck on execute_simulation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use indicatif::ProgressBar;
+use proposer_builder_seperation::blockchain_env;
+use proposer_builder_seperation::blockchain_env::*;
+use proposer_builder_seperation::entities::*;
+use proposer_builder_seperation::setup::init::*;
+use proposer_builder_seperation::simulation::*;
+use proposer_builder_seperation::stop::*;
+use rayon::prelude::*;
+
 fn main() {
-    println!("Hello World!");
+    let num_transactions = vec![60, 80, 100, 120, 140, 160];
+    let characteristics = vec![0.2, 0.6, 1.0];
+    let mev_builders = vec![1, 25, 49];
+    let rand_num_vec: Vec<f64> = get_random_numbers::<f64>(1000_000_00, 0.0, 1.0);
+    println!("Starting Simulations");
+    let pb = ProgressBar::new(
+        (num_transactions.len() * characteristics.len() * mev_builders.len()) as u64,
+    );
+
+    for num_transaction in num_transactions.iter() {
+        for characteristic in characteristics.iter() {
+            for mev_builder in mev_builders.iter() {
+                setup_and_execute_simulation(
+                    50,
+                    *mev_builder,
+                    *num_transaction,
+                    1000,
+                    100,
+                    *characteristic,
+                    format!("tammy_asked_for_these"),
+                    &rand_num_vec,
+                );
+                pb.inc(1);
+            }
+        }
+    }
+    pb.finish();
 }
--- src/simulation.rs
+++ src/simulation.rs
@@ -2,9 +2,12 @@ use crate::blockchain_env::*;
 use crate::entities::traits::Proposer;
 use crate::entities::{builder, proposer};
 use crate::setup::*;
+use indicatif::ProgressBar;
 use rand::thread_rng;
 use rand_distr::{Distribution, Normal, NormalError, Uniform};
+use rayon::prelude::*;
 use std::collections::HashSet;
+use std::thread::available_parallelism;
 
 pub fn execute_pos_simulation(
     num_blocks: u32,
@@ -57,11 +60,11 @@ pub fn execute_simulation(
     mut transaction_set: HashSet<transaction::Transaction>,
     random_number_vec: &Vec<f64>,
 ) -> Vec<block::Block> {
+    println!("Setup Simulation");
     let mut blockchain: Vec<block::Block> = vec![];
     let num_transactions = transaction_set.len() as u32;
-    let mut rng = thread_rng();
-    let uniform = Uniform::new(0, proposer_vec.len() as usize);
     for block_index in 1..=num_blocks {
+        print!("Block {} : ", block_index);
         for b in builder_vec.iter_mut() {
             match b {
                 builder::BuilderType::NormalBuilder(b) => {
@@ -72,18 +75,13 @@ pub fn execute_simulation(
                 }
             }
         }
-        let block_proposer = uniform.sample(&mut rng);
-        let mut proposed_block = proposer_vec[block_proposer].run_auction(
-            builder_vec,
-            100,
-            &blockchain,
-            random_number_vec,
-        );
-        proposer_vec[block_proposer].propose_block(
-            &proposer_vec[block_proposer],
-            &mut proposed_block,
-            100,
-        );
+        print!("transactions_collected || ");
+        //let block_proposer = uniform.sample(&mut rng);
+        let mut proposed_block =
+            proposer_vec[0].run_auction(builder_vec, 100, &blockchain, random_number_vec);
+        print!("auction complete || ");
+        proposer_vec[0].propose_block(&proposer_vec[0], &mut proposed_block, 100);
+        print!("block proposed || ");
         transaction::Transaction::clean_transaction_set(
             &mut transaction_set,
             &proposed_block.transactions,
@@ -94,8 +92,52 @@ pub fn execute_simulation(
             proposed_block.block_index.unwrap() + 1,
             &mut transaction_set,
         );
+        print!("transactions refilled || \n");
         //println!("{}", &proposed_block.get_block_index().unwrap());
         blockchain.push(proposed_block);
     }
     blockchain
+}
+
+pub fn setup_and_execute_simulation(
+    total_builders: u32,
+    mev_builders: u32,
+    num_transactions: u32,
+    num_blocks: u32,
+    block_size: u32,
+    characteristic: f64,
+    dir: String,
+    rand_num_vec: &Vec<f64>,
+) {
+    let mut builder_vec = init::initiate_builders(total_builders - mev_builders, characteristic);
+    builder_vec.append(&mut init::initiate_mev_builder(
+        mev_builders,
+        characteristic,
+    ));
+    let mut proposer_vec = init::initiate_proposers(5);
+
+    let transaction_set: HashSet<transaction::Transaction> =
+        init::initiate_transactions_default(num_transactions, 0);
+
+    println!("Executing Simulation");
+    let blockchain = execute_simulation(
+        num_blocks,
+        &mut builder_vec,
+        &mut proposer_vec,
+        transaction_set.clone(), // Clone required for parallel execution
+        &rand_num_vec,
+    );
+
+    let file_name = format!(
+        "data/{}/total_builders={}
+        mev_builders={}
+        num_transactions={}
+        num_blocks={}
+        block_size={}
+        characteristic={}",
+        dir, total_builders, mev_builders, num_transactions, num_blocks, block_size, characteristic
+    );
+
+    crate::stop::save_continuous_simulation_to_csv(&blockchain, &file_name)
+        .expect("save_blockchain_to_csv() failing from simple_pbs() test");
 }
